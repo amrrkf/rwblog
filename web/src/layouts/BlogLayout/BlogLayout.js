40c005987a7439087079a8f6c142384dbcf31211
amrrkf <amrrkf@gmail.com>
2020-06-15 14:28:02 +0100

    Initial commit

---
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -0,0 +1,17 @@
+import {Link, routes } from '@redwoodjs/router';
+
+const BlogLayout = ({ children }) => {
+  return <>
+  <h1><Link to={routes.home()}>Redwood blog</Link></h1>
+  <nav>
+    <ul>
+      <li>
+        <Link to={routes.about()}>About</Link>
+      </li>
+    </ul>
+  </nav>
+  <main>{children}</main>
+  </>
+}
+
+export default BlogLayout
---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout';
+
+const HomePage = () => {
+  return (
+    <BlogLayout>
+      <p>This is the home page!</p>
+    </BlogLayout>
+  )
+}
+
+export default HomePage
