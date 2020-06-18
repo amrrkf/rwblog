2b31f17da1062d614caf22904a2c1d54b42a9a78
amrrkf <amrrkf@gmail.com>
2020-06-19 00:56:08 +0100

    Complete contact page guide
    - ref: https://redwoodjs.com/tutorial/saving-data
    - The guide was for adding a contact table to the database in addition to its graphql queries and mutations.
    - Form was updated to reflect the error and loading of submissions to the server.
    - Added some form validation

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,13 +1,43 @@
-import { Form, TextField, Submit, TextAreaField, FieldError } from '@redwoodjs/web'
+import {
+  Form,
+  TextField,
+  Submit,
+  TextAreaField,
+  FieldError,
+  useMutation,
+  FormError
+ } from '@redwoodjs/web'
 import BlogLayout from "src/layouts/BlogLayout/BlogLayout";
 
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`;
+
 const ContactPage = () => {
+
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted: () => {
+      alert('Thank you for your submission!')
+    },
+  });
+
   const onSubmit = (data) => {
     console.log(data)
+    create({
+      variables: { input: data}
+    });
   }
 
   return <BlogLayout>
-    <Form onSubmit={onSubmit}>
+    <Form onSubmit={onSubmit} validation={{ mode: 'onBlur' }}>
+      <FormError
+        error={error}
+        wrapperStyle={{ color: 'red', backgroundColor: 'lavenderblush' }}
+      />
       <label htmlFor="name">Name</label>
       <TextField name="name" validation={{ required: true }} errorClassName="error" />
       <FieldError name="name" className="error"/>
@@ -19,7 +49,7 @@ const ContactPage = () => {
       <label htmlFor="message">Message</label>
       <TextAreaField name="message" validation={{ required: true }} errorClassName="error"/>
       <FieldError name="message" className="error"/>
-      <Submit>Save</Submit>
+      <Submit disabled={loading}>Save</Submit>
     </Form>
   </BlogLayout>
 }
